2264edf63095b395e636c33f0b5843f9b58fe6b8
phoenix <1903935168@qq.com>
2017-06-25 22:07:41 +0800

    main.cpp

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -1,22 +1,16 @@
 #include"app.h"
-
-
+#include<cstring>
 int main(int argc, char *argv[])
 {
 App a;
-a.jiami("1.huf","1.txt");
-a.jiemi("1.txt","1.huf");
-//cout<<getfilesize("1.txt");
-/*Tongjicell tongjis[3];
-tongjis[0].data='a';
-tongjis[0].weight=1;
-tongjis[1].data='b';
-tongjis[1].weight=1;
-tongjis[2].data='c';
-tongjis[2].weight=1;
-Huffmantree huff(tongjis,3);
-cout<<huff.gethuffcode('a')<<endl;
-cout<<huff.gethuffcode('b')<<endl;
-cout<<huff.gethuffcode('c')<<endl;*/
+if(argc!=4)
+{    cout<<"argv num is wrong use jia or jie ./huff jia dec src\n";return 1;}
+
+if(strcmp(argv[1],"jia")==0)
+      a.jiami(argv[2],argv[3]);
+    else if(strcmp(argv[1],"jie")==0)
+      a.jiemi(argv[2],argv[3]);
+    else
+    cout<<"argv is wrong use jia or jie ./huff jia dec src\n";
     return 0;
 }
